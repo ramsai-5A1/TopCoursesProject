d8496ed13c82cc44eb8e52b4b358089ca8b26002
Ramsai <ramsaicrony@gmail.com>
2023-05-04 20:51:25 +0530

    Added toast logic by adding useState

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,34 @@
-import React from "react";
-import { FcLike } from "react-icons/fc";
+import React, { useState } from "react";
+import { FcLike, FcLikePlaceholder } from "react-icons/fc";
+import { toast } from "react-toastify";
 import './Card.css';
 
 const Card = ({course}) => {
+
+    const [clicked, setClicked] = useState(true);
+
+    const fcLikeHandler = () => {
+        if(clicked) {
+            toast.success("Liked Successfully");
+        } else {
+            toast.warning("Like Removed");
+        }
+        setClicked(!clicked);
+    }
+
     return (
         <div className="topDiv">
             <div>
                 <img src={course.image.url} className='image'></img>
-                <div>
-                    <button>
-                        <FcLike fontSize="1.75rem"/>
+                <div className="fcLike">
+                    <button className="fcLikeButton" onClick={fcLikeHandler}>
+                        {!clicked ? <FcLike fontSize="1.75rem"/> : <FcLikePlaceholder fontSize="1.75rem"/>}
                     </button>
                 </div>
             </div>
 
             <div className="titleDiv">
-                <p>{course.title}</p>
+                <p className="title">{course.title}</p>
                 <p>{course.description}</p>
             </div>
         </div>
